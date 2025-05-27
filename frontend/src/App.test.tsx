99e7ab00b4215869b8af9c4b9603d4e1b7add774
Chris <ChristoWindow@gmail.com>
2025-05-27 14:44:35 -0400

    Added TypeScript + Updated testing
    Added TypeScript flexibility to allow to TSX and JSX.

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Welcome from './welcome.js'
-
+import Welcome from './welcome';
 
 /**********************************************************
  * Test whether components render without crashing
@@ -11,26 +10,21 @@ it('<App /> renders without crashing', () => {
 });
 
 it('<Welcome /> renders without crashing', () => {
-  render(<Welcome name = 'World'/>);
+  render(<Welcome name="World" />);
 });
-
 
 /**********************************************************
  * Test whether correct text is displayed
  *********************************************************/
 
 test('<Welcome /> displays name', () => {
-  render(<Welcome name = 'World'/>);
+  render(<Welcome name="World" />);
   const welcomeElement = screen.getByText(/Hello World/i);
   expect(welcomeElement).toBeInTheDocument();
 });
-
 
 test('Home page includes React Home Page', () => {
   render(<App />);
   const text = screen.getByText(/React Home Page/i);
   expect(text).toBeInTheDocument();
 });
-
-
-

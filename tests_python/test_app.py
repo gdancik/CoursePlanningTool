30367cd48bc2fc5ded2405fccc1ffd20aba15889
SencereRabel <Sencererabel@gmail.com>
2025-06-04 11:30:50 -0400

    Update test_app.py
    -Added if else to only setup getValue and updateValue test if the environmental variable is present.

--- tests_python/test_app.py
+++ tests_python/test_app.py
@@ -18,9 +18,12 @@ from backend.gs_editor import gsEditor
 @pytest.fixture(scope="module")
 def client():
     with app.test_client() as client:
-        setup_test_sheet()
-        yield client
-        delete_test_sheet()
+        if os.getenv('GS_CREDENTIALS_JSON') is not None:
+            setup_test_sheet()
+            yield client
+            delete_test_sheet()
+        else:
+            yield client
 
 def test_index(client):
     response = client.get('/')

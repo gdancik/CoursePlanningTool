98d6b579b8fa50a88cfb9d8975d8562e57ba7aa3
SencereRabel <Sencererabel@gmail.com>
2025-06-05 08:30:28 -0400

    updated course planning.py with placeholders from syllabus

--- backend/course_planning.py
+++ backend/course_planning.py
@@ -1,1 +1,3 @@
-columns = ['course_id','instructor_name_syllabus', 'crse_subj_syllabus', 'crse_number_syllabus']
+columns = ['course_id','instructor_name_syllabus', 'crse_subj_syllabus', 'crse_number_syllabus','days1_syllabus','times1_syllabus','location1_syllabus',
+           'days2_syllabus','times2_syllabus','location2_syllabus','term_syllabus','year_syllabus','email_syllabus','phone_syllabus',
+           'office_location_syllabus','office_hours_syllabus','additional_contact_info_syllabus','course_description_syllabus']

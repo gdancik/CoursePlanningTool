a20a02ac50a6a4806fdcd6341f3da78238de4d50
Chris <ChristoWindow@gmail.com>
2025-06-10 12:28:23 -0400

    Created Button for Course Modal
    Created the button for the course modal. Will need to create course modal

--- frontend/src/screens/CoursePage.tsx
+++ frontend/src/screens/CoursePage.tsx
@@ -1,40 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import StandardHeader from "../components/standardHeader";
 import ReusableButton from "../components/ReusableButton";
 import {FaPlus} from "react-icons/fa";
 import SafeIcon from "../utils/ComponentWrapper";
-import CourseModal from "../components/NewCourseModal";
-import './CoursePage.css'
 
 const CoursePage = () =>{
-    const[isModalOpen, setModalOpen] = useState(false);
-
-    const handleCreateCourse = (data: Record<string, string>) => {
-        console.log("CourseData submitted form modal :", data);
-        localStorage.setItem("newCourseData", JSON.stringify(data));
-        //TODO: Set up backend or save local
-    }
     return(
         <div>
             <StandardHeader/>
-            <div className="course-page">
-
-                <h1 className ="course-tool-head">Course Planning Tool</h1>
-
+            <div>
+                <h1>Course Planning Tool</h1>
                 <ReusableButton
                 label="New Course"
                 icon={<SafeIcon Icon={FaPlus}/>}
                 variant="primary"
-                onClick={() =>setModalOpen(true)}
-                className ="course-page-button"
                 />
             </div>
-            <CourseModal
-                isOpen={isModalOpen}
-                onClose={()=> setModalOpen(false)}
-                onCreate={handleCreateCourse}
-            />
-
         </div>
     );
 };

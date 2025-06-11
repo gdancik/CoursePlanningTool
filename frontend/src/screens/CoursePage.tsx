69467092d6d8562529d2f78506304d00eae51595
Chris <ChristoWindow@gmail.com>
2025-06-11 19:12:35 -0400

    Styled Course Planning Page

--- frontend/src/screens/CoursePage.tsx
+++ frontend/src/screens/CoursePage.tsx
@@ -4,6 +4,7 @@ import ReusableButton from "../components/ReusableButton";
 import {FaPlus} from "react-icons/fa";
 import SafeIcon from "../utils/ComponentWrapper";
 import CourseModal from "../components/NewCourseModal";
+import './CoursePage.css'
 
 const CoursePage = () =>{
     const[isModalOpen, setModalOpen] = useState(false);
@@ -16,13 +17,16 @@ const CoursePage = () =>{
     return(
         <div>
             <StandardHeader/>
-            <div>
-                <h1>Course Planning Tool</h1>
+            <div className="course-page">
+
+                <h1 className ="course-tool-head">Course Planning Tool</h1>
+
                 <ReusableButton
                 label="New Course"
                 icon={<SafeIcon Icon={FaPlus}/>}
                 variant="primary"
                 onClick={() =>setModalOpen(true)}
+                className ="course-page-button"
                 />
             </div>
             <CourseModal

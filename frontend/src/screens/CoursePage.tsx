06f005aa69eca43315ae945272215c139d5ff9b5
Chris <ChristoWindow@gmail.com>
2025-06-11 17:12:06 -0400

    Created Modal + button
    Modal has been created, Logic is needed to be done next

--- frontend/src/screens/CoursePage.tsx
+++ frontend/src/screens/CoursePage.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import StandardHeader from "../components/standardHeader";
 import ReusableButton from "../components/ReusableButton";
 import {FaPlus} from "react-icons/fa";
 import SafeIcon from "../utils/ComponentWrapper";
+import CourseModal from "../components/NewCourseModal";
 
 const CoursePage = () =>{
+    const[isModalOpen, setModalOpen] = useState(false);
+
+    const handleCreateCourse = (data: Record<string, string>) => {
+        console.log("CourseData submitted form modal :", data);
+        //TODO: Set up backend or save local
+    }
     return(
         <div>
             <StandardHeader/>
@@ -14,8 +21,15 @@ const CoursePage = () =>{
                 label="New Course"
                 icon={<SafeIcon Icon={FaPlus}/>}
                 variant="primary"
+                onClick={() =>setModalOpen(true)}
                 />
             </div>
+            <CourseModal
+                isOpen={isModalOpen}
+                onClose={()=> setModalOpen(false)}
+                onCreate={handleCreateCourse}
+            />
+
         </div>
     );
 };

70a05b0192c58c26a2c341f276dc44b5e993900d
SencereRabel <Sencererabel@gmail.com>
2025-06-17 13:29:01 -0400

    Updated syllabus_fetcher.py
    -Added create_syllabus_statment_page() function and helper functions that allow a document object to passed and adds all syllabus statements from the webpage to the end of the document

--- backend/syllabus_fetcher.py
+++ backend/syllabus_fetcher.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-
+from docx import Document
+from htmldocx import HtmlToDocx
+import re
 #%%
 def get_webpage(url) :
     ''' 
@@ -34,14 +36,9 @@ def extractFromAccordian(soup):
     for item in classes:
         rows = item.find_all(class_='row')
         for i in rows:
-            # print('===='*20)
-            # print(i.get_text(separator=" | ",strip=True))
-            header = i.find('h3').get_text(strip=True)
+            header = i.find('h3')
             content = i.find_all('div', class_='content')
-            all_content =""
-            for line in content:
-                all_content += line.get_text(strip=True)
-            headers_and_content[header] = all_content
+            headers_and_content[header] = content 
     return headers_and_content
 
 def getStatements(url):
@@ -54,3 +51,44 @@ def getStatements(url):
     '''
     soup = get_webpage(url)
     return extractFromAccordian(soup)
+
+def create_syllabus_statment_page(doc):
+    '''
+    Creates a syllabus statement page in the given Word document by fetching statements from a the syllabus statement website and adding them to the document.
+    Args:
+        doc (Document): The Word document object to which the syllabus statements will be added.
+    Returns:
+        None
+    '''
+    x = getStatements('https://www.easternct.edu/center-for-teaching-learning-and-assessment/course-design-resources/syllabus-statements.html')
+    for header, content in x.items():
+        header_string = str(header)
+        content_string = str(list(content)[0])
+        html_to_word_htmldocx(doc,header_string)
+        html_to_word_htmldocx(doc,content_string)  
+
+def html_to_word_htmldocx(doc,html_content):
+    '''
+    Converts HTML content to a Word document using the HtmlToDocx module and adds it to the specified document.
+    Args:
+        doc (Document): The Word document object to which the HTML content will be added.
+        html_content (str): The HTML content to be converted and added to the document.
+    Returns:
+        None
+    '''
+    cleaned_html = _strip_whitespace_from_div(html_content)
+    new_parser = HtmlToDocx()
+    new_parser.add_html_to_document(cleaned_html,doc)
+
+def _strip_whitespace_from_div(html_content):
+    '''
+    Strips unnecessary whitespace from HTML content, specifically around <p> and <div class="content"> tags.
+    Args:
+        html_content (str): The HTML content to be cleaned.
+    Returns:
+        str: Cleaned HTML content with unnecessary whitespace removed.
+    '''
+    stripped_html = re.sub(r'\s+(<p>)', r'\1', html_content)
+    stripped_html = re.sub(r'(<\/p>)\s+', r'\1', stripped_html)  
+    stripped_html = re.sub(r'(<\/div class="content">)\s+', r'\1', stripped_html)
+    return stripped_html

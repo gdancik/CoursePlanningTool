7a2820d67db90caf27954708fac13af7075ce9b6
Garrett <garrett.dancik@gmail.com>
2025-06-25 02:59:31 +0000

    Update wsgi for blueprint structure

--- wsgi.py
+++ wsgi.py
@@ -10,14 +10,15 @@ import sys
 
 import os
 from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/CoursePlanningTool/backend')  # adjust as appropriate
+project_folder = os.path.expanduser('~/CoursePlanningTool/backend/')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 
 
 # add your project directory to the sys.path
-project_home = '/home/gdancik/CoursePlanningTool/backend'
+project_home = '/home/gdancik/CoursePlanningTool'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
 # import flask app but need to call it "application" for WSGI to work
-from app import app as application  # noqa
+from backend import create_app
+application = create_app() # noqa

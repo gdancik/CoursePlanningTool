43220bd3b0d008d38192b90a50adcdd91326a40b
Chris <ChristoWindow@gmail.com>
2025-07-03 19:19:16 -0400

    Update api_integration_test.py

--- backend/tests/api_integration_test.py
+++ backend/tests/api_integration_test.py
@@ -11,4 +11,21 @@ print("Login Response:", login_response.json())
 get_response = session.post("https://gdancik.pythonanywhere.com/api/getSheet/")
 print("Course List:", get_response.json())
 
+json = {'dict_of_columns_and_vals' :
+
+            {'term_syllabus': 'Fall',
+
+             'year_syllabus': '2025',
+
+             'subj_code_syllabus': 'ART',
+
+             'crse_number_syllabus': '100'}
+
+        }
+
+
+
+response = session.post('https://gdancik.pythonanywhere.com/api/createNewCourse/', json = json)
+
+print(response.text)
 

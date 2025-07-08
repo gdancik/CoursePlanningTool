386e84557f2c349ade36623fba66ac3775b9b7a0
Garrett Dancik <gdancik@users.noreply.github.com>
2025-07-08 18:46:18 -0400

    Update axios.ts

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'https://gdancik.pythonanywhere.com/api/',
     withCredentials: true, // allows sending cookies for session auth
 });
 

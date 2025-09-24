667ed0caee4d6aa6d215bfbef8db17082bb17588
Garrett Dancik <gdancik@users.noreply.github.com>
2025-09-24 12:08:34 -0400

    Reorganized Alert, CheckboxGroup, and Information components, see test-page3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,22 @@ import '@fontsource/inter/400.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TestPageRunner from './TestPageRunner';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// Set the test page
-process.env.REACT_APP_TEST_PAGE = 'components';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <TestPageRunner />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

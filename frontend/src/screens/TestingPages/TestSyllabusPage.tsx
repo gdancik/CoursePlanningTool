a86c2146a7214501da49d21060b0d1cc601a9f74
Chris <100327446+cwindrow007@users.noreply.github.com>
2025-09-25 10:33:43 -0400

    TestJson Completed

--- frontend/src/screens/TestingPages/TestSyllabusPage.tsx
+++ frontend/src/screens/TestingPages/TestSyllabusPage.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import GeneratePageWrapper from "../../components/SyllabusComponents/SyllabusPageComponents/GeneratePageWrapper";
-//`import testPageJson from "../Data/test-page.json"
+import testPageJson from "../SyllabusView/Data/test-page.json";
 
 const TestSyllabusPage: React.FC = () => {
-    return <GeneratePageWrapper json={}/>
+    return <GeneratePageWrapper json={testPageJson.content} />;
 }
 
 export default TestSyllabusPage;

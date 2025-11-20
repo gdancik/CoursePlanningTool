1fc686d1360b24a341bfe129ff24b23537838db4
Garrett Dancik <gdancik@users.noreply.github.com>
2025-11-20 11:14:20 -0500

    Add gradeData prop to gradeTable

--- frontend/src/components/SyllabusComponents/Tables/gradeTable.js
+++ frontend/src/components/SyllabusComponents/Tables/gradeTable.js
@@ -5,6 +5,10 @@ import "./gradeTable.css";
  * @function GradeTable
  * @description The GradeTable component
  * @param props.id - the id of the table element
+ * @param props.data - data to load (leave undefined for no data)
+ * @details - if props.data is specified, it must be a nested array
+ *            with the second element of each row corresponding to 
+ *            the grade value. The grade letter is currently fixed.            
  * @returns {html} a grade table with 1 column for letter grades 
  *                 and an editable column for values.
  */
@@ -23,20 +27,26 @@ function GradeTable(props) {
     "F",
   ];
 
-  const gradeValue = [
-    "94-100%",
-    "90-93%",
-    "86-89%",
-    "82-85%",
-    "79-81%",
-    "75-78%",
-    "71-74%",
-    "67-70%",
-    "62-66%",
-    "55-61%",
-    "> 55%",
-  ];
+  let gradeValue = [
+      "94-100%",
+      "90-93%",
+      "86-89%",
+      "82-85%",
+      "79-81%",
+      "75-78%",
+      "71-74%",
+      "67-70%",
+      "62-66%",
+      "55-61%",
+      "> 55%",
+    ];
 
+  // process data
+  if (props.data !== undefined) {  
+    gradeValue = props.data.map( (x) => x[1]);
+    
+  }
+    
   return (
     <div>
       <h1>Grade Table</h1>

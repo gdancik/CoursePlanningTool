cfd5cb0c46027284e6f379d1e94919363eaf5cf6
Audrey <ChristoWindow@gmail.com>
2026-06-19 21:15:07 -0400

    Added image helpers to prevent rendering bugs on course id Change

--- frontend/src/components/SyllabusComponents/Image.tsx
+++ frontend/src/components/SyllabusComponents/Image.tsx
@@ -6,13 +6,20 @@ type ImageProps = {
     alt?: string;
     className?: string;
 }
+//Image Icon Helper Function
+export const getImageSource = (icon: string) => {
+    if (icon.startsWith("/") || icon.startsWith("http")) {
+        return icon;
+    }
+    return `/${icon}`
+}
 
 function Image({value, alt = '', className = ''}:ImageProps) {
     if (!value) return null;
 
     return(
         <img
-            src={value}
+            src={getImageSource(value)}
             alt={alt}
             className={className}
             loading = 'lazy'
--- frontend/src/screens/SyllabusView/Learning Outcomes/LearningOutcomesComponents.tsx
+++ frontend/src/screens/SyllabusView/Learning Outcomes/LearningOutcomesComponents.tsx
@@ -8,12 +8,15 @@ import AdditionalCompetenciesImage from "../../../assets/images/AdditionalCompet
 import KnowledgeTable from "../../../assets/images/Knowledge_table.png";
 import SkillsTable from "../../../assets/images/Skill_table.png";
 import AttitudeTable from "../../../assets/images/Attitudes_Table.png";
+import {getImageSource} from "../../../components/SyllabusComponents/Image";
 
 export interface CoreCompetencyInterface {
   icon: string;
   alt: string;
   content: string;
-}; 
+};
+
+
 
 export const FiveCoreCompetencies = ( { five }: { five: CoreCompetencyInterface[] }) => {
         return (
@@ -24,7 +27,7 @@ export const FiveCoreCompetencies = ( { five }: { five: CoreCompetencyInterface[
                 {five.map( (x) => {
                     return (                        
                             <div  key = {x.alt} className="core-competency-card">
-                                <img src={x.icon} alt={x.alt} className="core-icon"/>
+                                <img src={getImageSource(x.icon)} alt={x.alt} className="core-icon"/>
                                 <p>{x.content}</p>
                             </div>                    
                 

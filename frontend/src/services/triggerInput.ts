f59ea598450dbf7d9f59c66f4c01cef30e8ba019
Audrey <ChristoWindow@gmail.com>
2026-06-23 18:49:41 -0400

    Update triggerInput.ts

--- frontend/src/services/triggerInput.ts
+++ frontend/src/services/triggerInput.ts
@@ -2,7 +2,8 @@
   // used to trigger inputs on delete (e.g., for courseSchedule)
   export const triggerInput = () => {
     const textarea = document.querySelector('textarea');
-   
+
+
     // Create and dispatch an input event
     if (textarea) {
         const event = new Event('input', { bubbles: true });
